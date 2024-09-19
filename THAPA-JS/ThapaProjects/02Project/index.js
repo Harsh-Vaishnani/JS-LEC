616b30fea397ee3fde09870db19b2e5a6f83154e
Harsh-Vaishnani <harshvaishnani8@gmail.com>
2024-09-19 09:49:39 +0530

    JavaScript

--- THAPA-JS/ThapaProjects/02Project/index.js
+++ THAPA-JS/ThapaProjects/02Project/index.js
@@ -1,30 +1,40 @@
 let body = document.querySelector(".body");
-let data = document.querySelector("#inputData");
-let btn = document.querySelector(".btn");
+body.style.textAlign = "center";
 
-let ToDoFunction = () => {
-  console.log(data.value);
+let inputData = document.querySelector(".input");
+inputData.style.height = "25px";
+inputData.style.width = "250px";
+
+let btn = document.querySelector(".btn");
+btn.style.height = "25px";
+
+let myEvent = () => {
+  if (inputData.value !== "") {
+    let h2 = document.createElement("h2");
+    h2.innerText = inputData.value;
+    body.append(h2);
+    inputData.value = "";
+    let deleteBtn = document.createElement("button");
+    deleteBtn.innerText = "Delete";
+    body.append(deleteBtn);
+    deleteBtn.addEventListener("click", () => {
+      h2.remove();
+      deleteBtn.remove();
+      editBtn.remove();
+    });
+    let editBtn = document.createElement("button");
+    editBtn.innerText = "Edit";
+    body.append(editBtn);
+    editBtn.addEventListener("click", (e) => {
+      e.preventDefault();
+      inputData.value = h2.innerText;
+    });
+  } else {
+    alert("Serch Feild is Empty");
+  }
 };
 
 btn.addEventListener("click", (e) => {
   e.preventDefault();
-  ToDoFunction();
-
-  let newEle = document.createElement("h2");
-  newEle.innerText = data.value;
-  data.value = "";
-  body.append(newEle);
-
-  let btn2 = document.createElement("button");
-  btn2.innerText = "Delete";
-  console.log(btn2);
-  body.append(btn2);
-
-  btn2.addEventListener("click", (e) => {
-    e.preventDefault();
-    newEle.remove();
-    btn2.remove();
-  });
-
-
+  myEvent();
 });
